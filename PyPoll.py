f80d70474ec7a8aa8541152d9851836e4bfa8b14
Marsha Curtis <curt0230@gmail.com>
2022-04-07 16:23:17 -0500

    Completed module 3.4.5

---
+++ PyPoll.py
@@ -0,0 +1,36 @@
+# Data to retrieve:
+# 1. Total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. Total number of votes each candidate received
+# 4. Percentage of votes each candidate won
+# 5. The winner of the election based on popular vote
+# Resource path relative to the location of the .py script: Resources/election_results.csv
+
+# Assign a variable for the file to load and the path.
+
+import csv
+import os
+
+# Assign a variable to load a file from a path.
+file_to_load = os.path.join("Resources", "election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file.
+with open(file_to_load) as election_data:
+
+    # Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+    # Print each row in the CSV file.
+    for row in file_reader:
+        print(row[0])
+    
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+
+    # Write some data to the file.
+    txt_file.write("Counties in the election\n------------------------\nArapahoe\nDenver\nJefferson")
